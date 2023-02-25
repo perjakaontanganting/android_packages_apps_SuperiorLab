d886f3c17ffdc9af7c02e06f9354ce7cf825ad0a
Joker-V2 <nipinna0@gmail.com>
2023-02-25 18:03:07 +0100

    SuperiorLab: Remove netflix spoofing toggle
    Sync with https://github.com/SuperiorOS/android_frameworks_base/commit/271dbfafe5c8804d1e64e28ea1c37717c32be7a8
    
    Signed-off-by: Joker-V2 <nipinna0@gmail.com>

--- src/com/superior/lab/fragments/MiscSettings.java
+++ src/com/superior/lab/fragments/MiscSettings.java
@@ -36,7 +36,6 @@ import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.SwitchPreference;
 import android.provider.Settings;
 import com.android.settings.R;
-import com.android.settingslib.development.SystemPropPoker;
 
 import java.util.Arrays;
 import java.util.HashSet;
@@ -48,16 +47,13 @@ public class MiscSettings extends SettingsPreferenceFragment implements
 
     private static final String KEY_GAMES_SPOOF = "use_games_spoof";
     private static final String KEY_PHOTOS_SPOOF = "use_photos_spoof";
-    private static final String KEY_NETFLIX_SPOOF = "use_netflix_spoof";
     private static final String SMART_PIXELS = "smart_pixels";
 
     private static final String SYS_GAMES_SPOOF = "persist.sys.pixelprops.games";
     private static final String SYS_PHOTOS_SPOOF = "persist.sys.pixelprops.gphotos";
-    private static final String SYS_NETFLIX_SPOOF = "persist.sys.pixelprops.netflix";
 
     private SwitchPreference mGamesSpoof;
     private SwitchPreference mPhotosSpoof;
-    private SwitchPreference mNetFlixSpoof;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -73,10 +69,6 @@ public class MiscSettings extends SettingsPreferenceFragment implements
         mPhotosSpoof = (SwitchPreference) prefScreen.findPreference(KEY_PHOTOS_SPOOF);
         mPhotosSpoof.setChecked(SystemProperties.getBoolean(SYS_PHOTOS_SPOOF, true));
         mPhotosSpoof.setOnPreferenceChangeListener(this);
-
-        mNetFlixSpoof = (SwitchPreference) findPreference(KEY_NETFLIX_SPOOF);
-        mNetFlixSpoof.setChecked(SystemProperties.getBoolean(SYS_NETFLIX_SPOOF, false));
-        mNetFlixSpoof.setOnPreferenceChangeListener(this);
 
         updateSmartPixelsPreference();
     }
@@ -101,10 +93,6 @@ public class MiscSettings extends SettingsPreferenceFragment implements
         } else if (preference == mPhotosSpoof) {
             boolean value = (Boolean) objValue;
             SystemProperties.set(SYS_PHOTOS_SPOOF, value ? "true" : "false");
-            return true;
-        } else if (preference == mNetFlixSpoof) {
-            boolean value = (Boolean) objValue;
-            SystemProperties.set(SYS_NETFLIX_SPOOF, value ? "true" : "false");
             return true;
         }
         return false;
